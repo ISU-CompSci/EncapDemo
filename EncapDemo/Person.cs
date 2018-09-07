35ead2c1888222e94311c54d9789272068f200c7
holmjona <holmjona@isu.edu>
2018-09-06 20:27:15 -0600

    Added more attributes to Person Class. This should better fill out the object for future things we will use it for.

--- EncapDemo/Person.cs
+++ EncapDemo/Person.cs
@@ -7,21 +7,46 @@ using System.Threading.Tasks;
 namespace EncapDemo {
     class Person {
         #region Private Variables
-        public string _FirstName;
+        private string _FirstName;
         private string _LastName;
         private DateTime _DateOfBirth;
-        //private Colors _HairColor;
+        private Colors _HairColor;
         #endregion
 
-        #region
+        #region Enums
+        public enum Colors {
+            Black, // enums are stored as numbers and start at 0
+            Red,    // this has a value of 1 because 0 + 1 = 1. 
+            Brown = 10, // you can set the value.
+            Blonde // this now equals 11
+        }
+        #endregion
+
+        #region Public Properties
+        // Properties should always match the associated variable in name and data type.
+        /// <summary>
+        /// Get and Set the Person's First Name
+        /// </summary>
         public string FirstName {
             get {
                 return _FirstName;
             }
             set {
-                _FirstName = value;
+                // Make sure the first name is in the following format:
+                // Xxxxxxx
+                // Jack ( if jAck was entered)
+                // Jill ( if jill was entered)
+                // Mary ( if MARY was entered)
+                string newName = value[0].ToString().ToUpper();
+                if (value.Length > 1) {
+                    newName += value.Substring(1).ToLower();
+                }
+                _FirstName = newName;
             }
         }
+        /// <summary>
+        /// Get and Set the Person's First Name
+        /// </summary>
         public string LastName {
             get {
                 return _LastName;
@@ -29,6 +54,55 @@ namespace EncapDemo {
             set {
                 _LastName = value;
             }
+        }
+        /// <summary>
+        /// Get and Set the Person's Date of Birth
+        /// </summary>
+        public DateTime DateOfBirth {
+            get {
+                return _DateOfBirth;
+            }
+            set {
+                _DateOfBirth = value;
+            }
+        }
+        /// <summary>
+        /// Get and Set the Person's First Name
+        /// </summary>
+        /// <remarks>This uses the enum.</remarks>
+        public Colors HairColor {
+            get {
+                return _HairColor;
+            }
+            set {
+                _HairColor = value;
+            }
+        }
+        /// <summary>
+        /// Can create readonly properties
+        /// Get how old a person is based on their birth date.
+        /// </summary>
+        public int Age {
+            get{
+                // getters can include code. They are like a method.
+                // We do this to "protect our data" like said in class. 
+                TimeSpan howLongAlive = DateTime.Now - DateOfBirth;
+                double daysAlive = howLongAlive.TotalDays;
+                int yearsAlive = (int)(daysAlive / 365);
+                return yearsAlive;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Always have a description on methods.
+        /// Person shouts a message.
+        /// </summary>
+        /// <returns></returns>
+        public string Shout() {
+            return "Hey!";
         }
         #endregion
 
